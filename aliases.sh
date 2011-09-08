14c5864bfef5d50c531ec8cb86d246e511b96633
Stefan Blanke <greenarrow@users.sourceforge.net>
2011-09-08 10:44:06 +0100

    aliases.sh: Add alias git-strap
    Creates, adds all and commits to a git repo in the working directory.
    Handy for temporary comparison repos.

--- aliases.sh
+++ aliases.sh
@@ -1,10 +1,13 @@
 alias sl='ls'
 alias l='ls'
+
 alias dg='git diff'
 alias gd='git diff'
 alias gss='git status'
 alias tit='git'
 alias gg='gitg'
+alias git-strap='git init && git add . && git commit -a -m "Initial Commit"'
+
 alias ge='gedit'
 
 alias pie='PYTHONSTARTUP=$HOME/.greenshell/pie.py python'
