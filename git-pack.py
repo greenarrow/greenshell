ebd93e7a94fae8a4a5813bd4346c5d71b0309eda
Stefan Blanke <greenarrow@users.sourceforge.net>
2011-09-08 10:43:19 +0100

    git-pack.py: Improve error handling an display output filename
    Now checking all sub process error codes.

--- git-pack.py
+++ git-pack.py
@@ -7,18 +7,23 @@ import subprocess
 if __name__ == "__main__":
 	repo_name = os.path.split(os.getcwd())[1]
 
-	head_hash, err = subprocess.Popen(
+	p = subprocess.Popen(
 		"git log -1 --pretty=format:%h",
 		shell=True,
 		stdout=subprocess.PIPE
-	).communicate()
+	)
 
-	subprocess.Popen(
+	head_hash, err = p.communicate()
+	assert p.wait() == 0
+
+	assert subprocess.Popen(
 		"git archive -o ../%s-%s.tar HEAD" % (repo_name, head_hash),
 		shell=True,
-	).wait()
+	).wait() == 0
 
-	subprocess.Popen(
+	assert subprocess.Popen(
 		"gzip ../%s-%s.tar" % (repo_name, head_hash),
 		shell=True,
-	).wait()
+	).wait() == 0
+
+	print "written to ../%s-%s.tar.gz" % (repo_name, head_hash)
