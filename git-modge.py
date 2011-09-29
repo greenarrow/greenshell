497e4cc214374dac9b8358ffd8642ccaf7e42a1f
Stefan Blanke <greenarrow@users.sourceforge.net>
2011-09-29 11:24:35 +0100

    git-modge.py: Now opens files in more states and editor taken from env
    Editor is now taken from the environmental variable GS_EDITOR.

--- git-modge.py
+++ git-modge.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python
 
+"""
+Opens all changed files in the editor defined in GS_EDITOR or provided as an
+argument.
+"""
+
 import sys
+import os
 import re
 import subprocess
-
-
-DEFAULT_EDITOR = "gedit"
 
 
 if __name__ == "__main__":
 	if len(sys.argv) > 1:
 		editor = sys.argv[1]
 	else:
-		editor = DEFAULT_EDITOR
-	
-	map(
-		lambda i: subprocess.Popen("%s %s" % (editor, i), shell=True),
+		editor = os.environ["GS_EDITOR"]
+
+	success = all(map(
+		lambda i: subprocess.Popen(
+			"%s %s" % (editor, i[1]),
+			shell=True
+		).wait() == 0,
 		re.findall(
-			r' M (.+)',
-			subprocess.Popen('git status -s', shell=True, stdout=subprocess.PIPE).communicate()[0]
+			r"( M|UU) (.+)",
+			subprocess.Popen(
+				"git status -s",
+				shell=True,
+				stdout=subprocess.PIPE
+			).communicate()[0]
 		)
-	)
+	))
+
+	if not success:
+		sys.stderr.write("one or more commands failed")
+		sys.exit(1)
 
 
