7fad85288f0e0d5b087727a693fd8661f5ce2e08
Stefan Blanke <greenarrow@users.sourceforge.net>
2011-10-12 11:53:57 +0100

    green_cobalt.xml,install.sh: Add gedit colour scheme

--- install.sh
+++ install.sh
@@ -4,4 +4,7 @@ echo "source $HOME/.greenshell/greenrc.sh" >> $HOME/.bashrc
 ln -s $HOME/.greenshell/.gitconfig $HOME/.gitconfig
 ln -s $HOME/.greenshell/.gitignore-global $HOME/.gitignore-global
 
+# Install gedit colour scheme
+ln -s $HOME/.greenshell/green_cobalt.xml $HOME/.local/share/gtksourceview-2.0/styles/green_cobalt.xml
+
 echo "done"
