11ffba83ed7f690bf17f632c832b5faf18eb520c
Stefan Blanke <greenarrow@users.sourceforge.net>
2011-10-13 13:26:04 +0100

    green_cobalt.xml,install.sh: Move to new gedit directory

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ ln -s $HOME/.greenshell/.gitconfig $HOME/.gitconfig
 ln -s $HOME/.greenshell/.gitignore-global $HOME/.gitignore-global
 
 # Install gedit colour scheme
-ln -s $HOME/.greenshell/green_cobalt.xml $HOME/.local/share/gtksourceview-2.0/styles/green_cobalt.xml
+ln -s $HOME/.greenshell/gedit/green_cobalt.xml $HOME/.local/share/gtksourceview-2.0/styles/green_cobalt.xml
 
 echo "done"
