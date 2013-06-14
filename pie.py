52e1a3021d955134091b5301c3a2fe8e35f9a119
Stefan Blanke <greenarrow@users.sourceforge.net>
2013-06-14 14:35:54 +0100

    Cleanup pie Python environment

--- pie.py
+++ pie.py
@@ -9,31 +9,27 @@ import collections
 import time
 import subprocess
 import random
-import psycopg2
 import shutil
 import math
 
+try:
+    import psycopg2
+except:
+    pass
 
-def pc(dbname, host='localhost', user='postgres'):
+
+def pgc(dbname, host='localhost', user='postgres'):
 	return psycopg2.connect(database=dbname, user=user, host=host)
+
 
 def msc(dbname, host='localhost', user='root', password=None):
 	import MySQLdb
 	return MySQLdb.connect(db=dbname, user=user, host=host)
 
-def mc():
-	import pymongo
-	return pymongo.Connection('sys328', 27017)
 
 def clear():
 	print '\n' * int(os.popen('stty size', 'r').read().split()[0])
 
-def fdiapi(path=None):
-	if path is None:
-		path = os.getcwd()
-	sys.path.insert(0, path)
-	from fsDiskIndex import api
-	return api
 
 def diff(a, b):
     result = []
@@ -42,6 +38,7 @@ def diff(a, b):
     for i in range(len(a)):
             result.append((a[i] == b[i]) and '^' or ' ')
     return ''.join(result)
+
 
 def dictdiff(a, b):
     for key in set(a.iterkeys()) - set(b.iterkeys()):
