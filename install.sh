2a704134ba6eb028043d7731a595029074217479
Stefan Blanke <greenarrow@users.sourceforge.net>
2013-11-28 14:59:43 +0000

    Add gdbinit

--- install.sh
+++ install.sh
@@ -10,8 +10,10 @@ ln -s $HOME/.greenshell/.gitignore-global $HOME/.gitignore-global
 cd $HOME || exit 1
 mv .vim .vim.old
 mv .vimrc .vimrc.old
+mv .gdbinit .gdbinit.old
 ln -s $HOME/.greenshell/.vim $HOME/.vim
 ln -s $HOME/.greenshell/.vimrc $HOME/.vimrc
+ln -s $HOME/.greenshell/.gdbinit $HOME/.gdbinit
 
 mv $HOME/.config/Terminal/terminalrc $HOME/.config/Terminal/terminalrc.old
 ln -s $HOME/.greenshell/terminalrc $HOME/.config/Terminal/terminalrc
