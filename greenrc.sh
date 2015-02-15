c4408be05b1f81d58fc6c3e4eac6ad70e1434c31
Stefan Blanke <greenarrow@users.sourceforge.net>
2015-02-15 16:22:57 +0000

    Tidy

--- greenrc.sh
+++ greenrc.sh
@@ -2,10 +2,5 @@ source $HOME/.greenshell/aliases.sh
 
 export PATH=$HOME/.greenshell/bin:$PATH
 
-export WEATHER_URL="http://www.metoffice.gov.uk/weather/uk/se/heathrow_latest_weather.html"
-
-#export HISTCONTROL=erasedups
 export HISTSIZE=10000
 shopt -s histappend
-
-
